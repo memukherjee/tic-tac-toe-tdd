8ab6ca855eb6fc2ba661c4d1855654bf32a2b1ba
Akash Mukherjee <akash3.11.2000@gmail.com>
2024-04-29 06:01:35 +0530

    Status text added

--- src/component/Board.jsx
+++ src/component/Board.jsx
@@ -1,11 +1,16 @@
 export default function Board() {
     return (
-        <div className="board">
-            {[...Array(9).keys()].map((i) => (
-                <button key={i} className="square" data-testid="square">
-                    {i}
-                </button>
-            ))}
-        </div>
+        <>
+            <div className="status" data-testid="status">
+                Next player: X
+            </div>
+            <div className="board" data-testid="board">
+                {[...Array(9).keys()].map((i) => (
+                    <button key={i} className="square" data-testid="square">
+                        {i}
+                    </button>
+                ))}
+            </div>
+        </>
     );
 }
